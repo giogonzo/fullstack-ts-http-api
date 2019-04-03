ea5971eee7dc1eaaf581fea569afc376873c5a76
Giovanni Gonzaga <giovanni@buildo.io>
2019-04-03 15:46:01 +0200

    service returning Option

--- api/src/service.ts
+++ api/src/service.ts
@@ -1,17 +1,20 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import { range } from 'fp-ts/lib/Array'
+import { range, catOptions } from 'fp-ts/lib/Array'
 import { Post } from './model'
+import { Option, fromNullable } from 'fp-ts/lib/Option'
 
 const posts = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '../data/posts.json'), 'utf8')
 )
 
-export function getById(id: string): Promise<Post> {
-  return Promise.resolve({
-    ...posts[id],
-    date: new Date(posts[id].date)
-  })
+export function getById(id: string): Promise<Option<Post>> {
+  return Promise.resolve(
+    fromNullable(posts[id]).map(post => ({
+      ...post,
+      date: new Date(post.date)
+    }))
+  )
 }
 
 export function list(count: number): Promise<Array<Post>> {
@@ -19,5 +22,5 @@ export function list(count: number): Promise<Array<Post>> {
     range(1, count)
       .map(String)
       .map(getById)
-  )
+  ).then(catOptions)
 }
